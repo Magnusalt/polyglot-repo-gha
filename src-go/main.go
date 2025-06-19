459c7eb2ce53fde17a61c4e7c942d4501e3735f4
Magnus Altgard <magnus.altgard@gmail.com>
2025-06-19 16:20:59 +0200

    added som hello worlds

---
+++ src-go/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	
+	// This is a placeholder for the main function.
+	// You can add your code here to implement the desired functionality.
+	
+	// Example: Print a message to the console
+	println("Hello, World!")
+	
+	// Note: Make sure to import necessary packages if you use any functions or types from them.
+}
---
+++ src-node/main.js
@@ -0,0 +1,5 @@
+function main(params) {
+    console.log("Hello, World!");
+}
+
+main();

917f6b669e3166a09f52536ac91e23c847e300aa
Magnus Altgard <magnus.altgard@gmail.com>
2025-06-26 11:00:13 +0200

    adds stupid code to test cyclomatic complexity scan

--- src-csharp/Program.cs
+++ src-csharp/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// this is just to test cyclomatic complexity scanning during build.
+for (int i = 0; i < 100; i++)
+{
+    if (i % 6 == 0)
+    {
+        while (i % 6 == 0)
+        {
+            i++;
+        }
+    }
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    else
+    {
+        var y = i switch
+        {
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            _ => "" switch
+            {
+                "" => "empty",
+                _ => "default"
+            }
+        };
+    }
+}
